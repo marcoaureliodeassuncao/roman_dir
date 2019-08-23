b2398fece833797f4909fadbe360a0756c4a8fdd
Marco Aurélio <marco.assuncao@semantix.com.br>
2019-08-23 15:38:48 -0300

    testes separados

--- test_roman.py
+++ test_roman.py
@@ -9,21 +9,51 @@ num_romano_2 = 'II'
 
 class Tester(unittest.TestCase):
 
-	def test_valida_numero(self):
+	def test_valida_numero_positivo(self):
 		self.assertTrue(valida_numero('20'))
+		pass
+
+	def test_valida_numero_maximo_permitido(self):
 		self.assertTrue(valida_numero('3999'))
+		pass
+
+	def test_valida_numero_negativo(self):
 		self.assertFalse(valida_numero('-1'))
+		pass
+
+	def test_valida_numero_com_letras(self):
 		self.assertFalse(valida_numero('XX'))
+		pass
+
+	def test_valida_numero_decimal(self):
 		self.assertTrue(valida_numero('2.7'))
+		pass
+
+	def test_valida_numer_zero(self):
 		self.assertFalse(valida_numero('0'))
 		pass
 
 	def test_numero_para_romano(self):
 		self.assertEqual(numero_para_romano('1'), num_romano_1)
+		pass
+
+	def test_numero_para_romano_maximo_permitido(self):
 		self.assertEqual(numero_para_romano('3999'), num_romano_3999)
+		pass
+
+	def test_numero_para_romano_negativo(self):
 		self.assertFalse(numero_para_romano('-1'))
+		pass
+
+	def test_numero_para_romano_letras(self):
 		self.assertFalse(numero_para_romano('pizza'))
+		pass
+
+	def test_numero_para_romano_decimal(self):
 		self.assertEqual(numero_para_romano('2.5'), num_romano_2)
+		pass
+
+	def test_numero_para_romano_zero(self):
 		self.assertFalse(numero_para_romano('0'))
 		pass
 
